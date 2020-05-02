e871d36a347798faea75163d8e08fdac414c1412
Cristopher <cristopher.correo@gmail.com>
2020-05-02 21:28:03 +0100

    testing travis

--- src/component/App.js
+++ src/component/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar'
 import ImageList from './ImageList'
-
-class App extends React.Component { 
-  state = {images: []}
+// trying to trigger travis
+class App extends React.Component {
+  state = { images: [] }
   onSearchBarSubmit = (text) => {
-    unsplash.get('/search/photos', {params: {query: text}} )
-      .then(({data}) => this.setState({images: data.results}))
+    unsplash.get('/search/photos', { params: { query: text } })
+      .then(({ data }) => this.setState({ images: data.results }))
   }
 
-  render () {
+  render() {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onSearchBarSubmit} />
-        <ImageList images={this.state.images}/>
+        <ImageList images={this.state.images} />
       </div>
     );
   }
